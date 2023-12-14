b12e96abf285bfc5048ef33bf1d333362d466a3c
mjstarrett <mjstarrett@gmail.com>
2023-12-14 15:42:53 -0800

    Working on Generic Pointing in SNL_Workshop scene

---
+++ Assets/Landmarks/Scripts/ExperimentTasks/GenericPointing.cs
@@ -0,0 +1,84 @@
+﻿/*
+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GenericPointing : ExperimentTask
+{
+    [Header("Task-specific Properties")]
+    [TextArea] public string prompt_text = "Point to the {0}.";
+    public List<ObjectList> prompt_elements = new List<ObjectList>();
+    private string prompt;
+
+    public override void startTask()
+    {
+        TASK_START();
+
+        // LEAVE BLANK
+    }
+
+
+    public override void TASK_START()
+    {
+        if (!manager) Start();
+        base.startTask();
+
+        if (skip)
+        {
+            log.log("INFO    skip task    " + name, 1);
+            return;
+        }
+
+        // Configure the prompt for the HUD
+        foreach (var element in prompt_elements)
+        {
+            try
+            {
+                prompt = string.Format(prompt_text, element.currentObject().name);
+                element.incrementCurrent();
+                Debug.Log("INCREMENTED!");
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError("Possible mismatch between prompt elements specified and elements provided");
+            }
+            
+        }
+        Debug.Log(prompt_text);
+        Debug.Log(prompt);
+        hud.setMessage(prompt);
+        hud.showOnlyHUD();
+    }
+
+
+    public override bool updateTask()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) return true;
+        else return false;
+
+        // WRITE TASK UPDATE CODE HERE
+    }
+
+
+    public override void endTask()
+    {
+        TASK_END();
+
+        // LEAVE BLANK
+    }
+
+
+    public override void TASK_END()
+    {
+        base.endTask();
+
+        // WRITE TASK EXIT CODE HERE
+        Debug.Log("Done!");
+
+    }
+
+}
